7a944ffcc1a324a2923797449ae89d6c20af08e3
Sofik Sheikh <sofikshaikh2000@gmail.com>
2024-07-09 00:27:15 +0530

    routes changed

--- src/controller/permission.js
+++ src/controller/permission.js
@@ -5,22 +5,16 @@ const checkLib = require("../libs/checkLib");
 
 const serverPermission = async (req, res) => {
     try {
-        // Fetch the permission document
         let permission = await Permission.findOne({});
 
         if (!permission) {
-            // Create a new permission document if it doesn't exist
             permission = new Permission({
-                permission: true // or false, based on your initial requirement
+                permission: true 
             });
         } else {
-            // Toggle the permission value if the document exists
             permission.permission = !permission.permission;
         }
-
-        // Save the permission document
         await permission.save();
-
         const message = permission.permission ? "Permission is now true" : "Permission is now false";
         const apiResponse = responseLib.generate(true, message, {});
         res.status(200).send(apiResponse);
